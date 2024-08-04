05b5a4db91dd66bf1d4da6166a86af8759245b6b
navulurisatishreddy <navulurisatishreddy4728@gmail.com>
2024-08-04 21:13:17 +0530

    Revert "java files modified"
    This reverts commit 0f458cd983f3095d43aed63df12135776da6a852.

--- src/main/java/in/ashokit/App.java
+++ src/main/java/in/ashokit/App.java
@@ -8,9 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-       int a=10;
-       int b=20;
-       System.out.println(a+b);
-
+        System.out.println( "Hello World!" );
     }
 }
